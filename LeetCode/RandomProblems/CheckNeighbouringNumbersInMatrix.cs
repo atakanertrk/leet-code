d37fa6005cabd1a184a67d473b464b9e61e65900
atakan ertürk <atakanertrk@hotmail.com>
2024-12-05 22:23:12 +0300

    CheckNeighbouringNumbersInMatrix print

--- LeetCode/RandomProblems/CheckNeighbouringNumbersInMatrix.cs
+++ LeetCode/RandomProblems/CheckNeighbouringNumbersInMatrix.cs
@@ -19,13 +19,65 @@ namespace LeetCodeConsoleApp
          */
         public override void Run()
         {
-            int[,] matrix = 
-            { 
-                { 9, 4 }, 
-                { 6, 3 }, 
+            Console.WriteLine(
+           """
+               Calculating Neighbours For;
+                { 9, 4 }
+                { 6, 3 }
+                { 5, 8 }
+
+           """);
+            int[,] matrix =
+            {
+                { 9, 4 },
+                { 6, 3 },
                 { 5, 8 }
             };
+            var neighbourList = FindNeighbours(matrix);
+            PrintNeighbourRelationship(neighbourList);
 
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(
+    """
+               Calculating Neighbours For;
+                { 9, 77, 4, 12 }
+                { 6, 11, 3, 32 }
+                { 5, 23, 8, 16 }
+                { 89, 42, 55, 76 }
+
+          """);
+            int[,] matrix2 = 
+            {
+                { 9, 77, 4, 12 },
+                { 6, 11, 3, 32 },
+                { 5, 23, 8, 16 },
+                { 89, 42, 55, 76 }
+            };
+            var neighbourList2 = FindNeighbours(matrix2);
+            PrintNeighbourRelationship(neighbourList2);
+        }
+
+        private void PrintNeighbourRelationship(List<Neighbour> neighboursList)
+        {
+            foreach (var neighbour in neighboursList)
+            {
+                Console.WriteLine(neighbour.Value);
+                PrintNeighboursOfValue(neighbour);
+            }
+        }
+
+        private void PrintNeighboursOfValue(Neighbour neighbour)
+        {
+            foreach (var value in neighbour.Neighbours)
+            {
+                Console.WriteLine($"  -{value}");
+            }
+        }
+
+        private List<Neighbour> FindNeighbours(int[,] matrix)
+        {
             // my assumption is each element is neighbour of its +/- 1 on both x and y axis
             // therefore I create Neighbour class and List of it to contain neigbour relationship
             var neighboursList = new List<Neighbour>();
@@ -56,6 +108,7 @@ namespace LeetCodeConsoleApp
                     neighboursList.Add(neighbour);
                 }
             }
+            return neighboursList;
         }
 
         private class Neighbour
